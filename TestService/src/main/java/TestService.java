664973013c561e31394945b5a018430511d045a7
kvn <v.kudryavtsev@dev.untill.com>
2017-09-22 16:02:07 +0300

    TestService: doRun

--- TestService/src/main/java/TestService.java
+++ TestService/src/main/java/TestService.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.StringTokenizer;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Logger;
@@ -12,7 +14,7 @@ public class TestService {
 
 	public static void main(String[] args) {
 		try {
-			Handler handler = new FileHandler(new File(homePath, TestService.class.getSimpleName()+ ".log").getPath(), true);
+			Handler handler = new FileHandler(new File(homePath, TestService.class.getSimpleName()+ "%u.log").getPath(), true);
 			handler.setFormatter(new SimpleFormatter());
 			log.addHandler(handler);
 		} catch (SecurityException | IOException e) {
@@ -42,12 +44,45 @@ public class TestService {
 				log.info("doStop");
 				stop = true;
 			}
+			File doRun = new File(homePath, "doRun");
+			if (doRun.exists()) {
+				String command;
+				try {
+					command = new String(Files.readAllBytes(doRun.toPath()));
+				} catch (IOException e) {
+					log.throwing("TestService", "start", e);
+					continue;
+				}
+				if (doRun.delete() && !command.isEmpty()) {
+					log.info("doRun " + command);
+					doRun(command);
+				}
+			}
 			log.info("running");
 		}
 		log.info("start end");
 	}
 
+	private static void doRun(String command) {
+        StringTokenizer st = new StringTokenizer(command);
+        String[] cmdarray = new String[st.countTokens()];
+        for (int i = 0; st.hasMoreTokens(); i++)
+            cmdarray[i] = st.nextToken();
+		ProcessBuilder pb = new ProcessBuilder(cmdarray)
+				.inheritIO();
+		try {
+			pb.start();
+		} catch (IOException e) {
+			log.throwing("TestService", "doRun", e);
+		}
+
+	}
+
 	private static void stop() {
+		if (new File(homePath, "unstoppable").exists()) {
+			log.info("stop ignore");
+			return;
+		}
 		log.info("stop");
 		stop = true;
 	}
