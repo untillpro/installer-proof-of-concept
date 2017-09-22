9f0f26999c5e1da852745aa3d52a33a43238a32d
kvn <v.kudryavtsev@dev.untill.com>
2017-09-22 15:41:21 +0300

    next step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Main {
 
 	public static void main(String[] args) {
+		String srcPath = args.length > 1 ? args[1]
+				: new File(getJarFile().getParentFile().getParentFile().getParentFile(), "TestService/build/windows-service").getPath();
 		if (args.length > 0) {
 			if ("install".equals(args[0])) {
-				install();
+				install(srcPath);
 			} else if ("uninstall".equals(args[0])) {
 				uninstall();
 			} else if ("upgrade".equals(args[0])) {
-				upgrade();
-			} else if ("reboot&upgrade".equals(args[0])) {
-				rebootAndUpgrade();
+				upgrade(srcPath);
+			} else if ("rebootAndUpgrade".equals(args[0])) {
+				rebootAndUpgrade(srcPath);
 			} else if ("upgradeAfterReboot".equals(args[0])) {
-				upgradeAfterReboot();
+				upgradeAfterReboot(srcPath);
 			} else
 				printUsage();
 		} else
@@ -20,39 +26,92 @@ public class Main {
 
 	private static void printUsage() {
 		System.out.println("Usage:");
-		System.out.println("\t" + Main.class.getSimpleName() + " install|uninstall|upgrade|reboot&upgrade");
+		System.out.println("\t java -jar " + getJarFile().getName() + " install|uninstall|upgrade|rebootAndUpgrade [srcPath]");
 		System.exit(1);
 	}
 
-	private static void install() {
-		if (!TestServiceInstaller.install())
+	private static void install(String srcPath) {
+		if (!TestServiceInstaller.install(srcPath)) {
+			System.err.println("Error: install");
 			System.exit(2);
+		}
 	}
 
 	private static void uninstall() {
-		if (!TestServiceInstaller.uninstall())
-			System.exit(2);
+		if (!TestServiceInstaller.uninstall()) {
+			System.err.println("Error: uninstall");
+			System.exit(3);
+		}
 	}
 
-	private static void upgrade() {
-		if (!TestServiceInstaller.uninstall())
-			System.exit(2);
-		if (!TestServiceInstaller.install())
-			System.exit(2);
+	private static void upgrade(String srcPath) {
+		if (!TestServiceInstaller.uninstall()) {
+			System.err.println("Error: upgrade (uninstall)");
+			System.exit(4);
+		}
+		// TODO upgrade
+		if (!TestServiceInstaller.install(srcPath)) {
+			System.err.println("Error: upgrade (install)");
+			System.exit(5);
+		}
 	}
 
-	private static void rebootAndUpgrade() {
-		TestServiceInstaller.disableService();
-		// TODO create task: schtasks /Create /TN TestTask /TR "java -jar \"<jarPath>\" upgradeAfterReboot" /SC ONSTART /RU SYSTEM
-		// TODO reboot
+	private static void rebootAndUpgrade(String srcPath) {
+		String jarPath = getJarFile().getPath();
+		if (jarPath == null || !jarPath.toLowerCase().endsWith(".jar")) {
+			System.err.println("Error: this command is available only from jar");
+			System.exit(6);
+		}
+		// TODO check for elevated
+		// disable service
+		if (!TestServiceInstaller.disableService()) {
+			System.err.println("Error: disableService");
+			System.exit(7);
+		}
+		// create task
+		int res = exec("schtasks", "/Create", "/TN", "TestTask",
+				"/TR", "java -jar \\\"" + jarPath + "\\\" upgradeAfterReboot",
+				"/SC", "ONSTART", "/RU", "SYSTEM");
+		if (res != 0) {
+			System.err.println(String.format("Error: create task (%d)", res));
+			System.exit(8);
+		}
+		// reboot
+		res = exec("shutdown", "-r");
+		if (res != 0) {
+			System.err.println(String.format("Error: reboot (%d)", res));
+			System.exit(9);
+		}
 	}
 
-	private static void upgradeAfterReboot() {
-		// TODO remove task: schtasks /Delete /F /TN TestTask
-		if (!TestServiceInstaller.uninstall())
-			System.exit(2);
-		if (!TestServiceInstaller.install())
-			System.exit(2);
+	private static void upgradeAfterReboot(String srcPath) {
+		// delete task
+		int res = exec("schtasks", "/Delete", "/F", "/TN", "TestTask");
+		if (res != 0) {
+			System.err.println(String.format("Error: delete task (%d)", res));
+			System.exit(10);
+		}
+		upgrade(srcPath);
 	}
 
+	public static File getJarFile() {
+		try {
+			return new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static int exec(String... command) {
+		ProcessBuilder pb = new ProcessBuilder(command)
+				.inheritIO();
+		try {
+			Process p = pb.start();
+			return p.waitFor();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
 }
--- src/main/java/TestServiceInstaller.java
+++ src/main/java/TestServiceInstaller.java
@@ -1,19 +1,66 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class TestServiceInstaller {
 
-	public static boolean install() {
-		// TODO
-		return false;
+	public static boolean install(String srcPath) {
+		File dstDir = new File(Main.getJarFile().getParentFile(), "TestService");
+		if (new File(dstDir, "lib/TestService.jar").exists()) {
+			System.err.println("install: warning: already installed");
+			return false;
+		}
+		try {
+			new File(dstDir, "lib").mkdirs();
+			Files.copy(Paths.get(srcPath, "TestService.exe"), dstDir.toPath().resolve("TestService.exe"));
+			Files.copy(Paths.get(srcPath, "TestService-install.bat"), dstDir.toPath().resolve("TestService-install.bat"));
+			Files.copy(Paths.get(srcPath, "TestService-uninstall.bat"), dstDir.toPath().resolve("TestService-uninstall.bat"));
+			Files.copy(Paths.get(srcPath, "lib", "TestService.jar"), dstDir.toPath().resolve("lib").resolve("TestService.jar"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("install: error: copy");
+			return false;
+		}
+		if (Main.exec("cmd", "/c", new File(dstDir, "TestService-install.bat").getPath()) != 0) {
+			System.err.println("install: error: TestService-install.bat");
+			return false;
+		}
+		if (Main.exec(new File(dstDir, "TestService.exe").getPath(), "start") != 0) {
+			System.err.println("install: error: TestService start");
+			return false;
+		}
+		return true;
 	}
 
 	public static boolean uninstall() {
-		// TODO
-		return false;
+		File dstDir = new File(Main.getJarFile().getParentFile(), "TestService");
+		if (!new File(dstDir, "lib/TestService.jar").exists()) {
+			System.err.println("uninstall: info: not installed");
+			return false;
+		}
+		if (Main.exec("cmd", "/c", new File(dstDir, "TestService-uninstall.bat").getPath()) != 0) {
+			System.err.println("uninstall: error: TestService-uninstall.bat");
+			return false;
+		}
+		new File(dstDir, "TestService.exe").delete();
+		new File(dstDir, "TestServicew.exe").delete();
+		new File(dstDir, "TestService-install.bat").delete();
+		new File(dstDir, "TestService-uninstall.bat").delete();
+		new File(dstDir, "TestService-install.bat").delete();
+		new File(dstDir, "lib/TestService.jar").delete();
+		new File(dstDir, "lib").delete();
+		dstDir.delete();
+		return true;
 	}
 
 	public static boolean disableService() {
-		// TODO
-		return false;
+		File dstDir = new File(Main.getJarFile().getParentFile(), "TestService");
+		if (Main.exec(new File(dstDir, "TestService.exe").getPath(), "update", "--Startup=manual") != 0) {
+			System.err.println("disableService: error: TestService update --Startup=manual");
+			return false;
+		}
+		return true;
 	}
 
 }
- 
